9ba9b784d0976936febfb4e86f221308b3efaa38
azanra <112457939+azanra@users.noreply.github.com>
2024-06-09 09:29:51 +0700

    Add function to create grid square

---
+++ script.js
@@ -0,0 +1,18 @@
+let rowAmount = 16;
+let columnAmount = 16;
+
+const container = document.querySelector('.container');
+
+function createGrid() {
+    for(let i = 0; i < rowAmount; i++){
+        for(let j = 0; j < columnAmount; j++){
+            const gridSquare = document.createElement('div');
+            gridSquare.classList.add('.gridSquare');
+            container.appendChild(gridSquare);
+        }
+    }
+}
+
+defaultGrid();
+console.log(rowAmount, columnAmount);
+createGrid(rowAmount, columnAmount);
